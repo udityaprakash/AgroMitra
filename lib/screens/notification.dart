c1c1ee155d8c5645e278f60bfcbc97c20683ae2b
Uditya Prakash <udityaprakash983@gmail.com>
2024-12-11 12:26:06 +0530

    commited complete screen of notifications

--- lib/screens/notification.dart
+++ lib/screens/notification.dart
@@ -1,4 +1,5 @@
 import 'package:agromitra/constant/color.dart';
+import 'package:agromitra/functions/loading.dart';
 import 'package:agromitra/utils/ui/custom-text.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -20,10 +21,12 @@ class _NotificationScreenState extends State<NotificationScreen> {
     fetchNotifications();
   }
 
+  // Fetch notifications from the API
   Future<void> fetchNotifications() async {
-    const String url = 'https://bckend.com/post';
+    const String url = 'https://sih-agromitra-new-server-psi.vercel.app/notifications'; // API URL
     try {
       final response = await http.get(Uri.parse(url));
+
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
         if (data['success'] == true) {
@@ -31,7 +34,18 @@ class _NotificationScreenState extends State<NotificationScreen> {
             notifications = data['notifications'];
             isLoading = false;
           });
+        } else {
+          setState(() {
+            isLoading = false;
+          });
+          print('Error: API success flag is false');
         }
+      } else {
+        // Handle unsuccessful API response (non-200 status)
+        setState(() {
+          isLoading = false;
+        });
+        print('Failed to load notifications. Status code: ${response.statusCode}');
       }
     } catch (e) {
       setState(() {
@@ -41,19 +55,33 @@ class _NotificationScreenState extends State<NotificationScreen> {
     }
   }
 
-  Color getPriorityColor(String priority) {
-    switch (priority.toLowerCase()) {
-      case 'high':
+  Future<void> _performRefresh() async {
+    setState(() {
+      isLoading = true;
+    });
+    await fetchNotifications();
+    // await _getRecommendedCrops();
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  // Get color based on notification priority
+  Color getPriorityColor(int priority) {
+    switch (priority) {
+      case 1:
         return Colors.red.shade100;
-      case 'medium':
+      case 2:
         return Colors.orange.shade100;
-      case 'low':
+      case 3:
         return Colors.grey.shade200;
       default:
         return Colors.white;
     }
   }
 
+  // Get the icon based on the type of notification
   IconData getIcon(String icon) {
     switch (icon) {
       case 'weather':
@@ -74,63 +102,73 @@ class _NotificationScreenState extends State<NotificationScreen> {
     return Scaffold(
       backgroundColor: AppColors.newbackground,
       appBar: AppBar(
-        title: CustomTextWidget(text: AppLocalizations.of(context)!.notification, textColor: AppColors.white, fontSize: 20,),
+        title: CustomTextWidget(
+          text: AppLocalizations.of(context)!.notification,
+          textColor: AppColors.white,
+          fontSize: 20,
+        ),
         centerTitle: true,
         backgroundColor: AppColors.primary,
       ),
-      body: isLoading
-          ? Center(child: CircularProgressIndicator())
-          : notifications.isEmpty
-              ? Center(child: Text('No notifications available'))
-              : Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListView.builder(
-                    itemCount: notifications.length,
-                    itemBuilder: (context, index) {
-                      final notification = notifications[index];
-                      final tags = notification['tags'] ?? [];
-                      return Card(
-                        color: getPriorityColor(notification['priority'] ?? 'low'),
-                        elevation: 3,
-                        margin: const EdgeInsets.symmetric(vertical: 6),
-                        child: ListTile(
-                          leading: CircleAvatar(
-                            backgroundColor: Colors.white,
-                            child: Icon(
-                              getIcon(notification['icon'] ?? ''),
-                              color: Colors.black54,
+      body: RefreshIndicator(
+        onRefresh: _performRefresh,
+        
+        child: isLoading
+            ? Center(child: loading())
+            : notifications.isEmpty
+                ? Center(child: CustomTextWidget(text: AppLocalizations.of(context)!.notification , textColor: AppColors.textPrimary)) // Text localization
+                : Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListView.builder(
+                      itemCount: notifications.length,
+                      itemBuilder: (context, index) {
+                        final notification = notifications[index];
+                        final tags = notification['tags'] ?? [];
+                        return Card(
+                          color: getPriorityColor(notification['priority'] ?? 'low'),
+                          elevation: 3,
+                          margin: const EdgeInsets.symmetric(vertical: 6),
+                          child: ListTile(
+                            minTileHeight: 100,
+                            leading: CircleAvatar(
+                              backgroundColor: Colors.white,
+                              child: Icon(
+                                getIcon(notification['icon'] ?? ''),
+                                color: Colors.black54,
+                              ),
+                            ),
+                            title: Text(
+                              notification['title'] ?? '',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                              ),
+                            ),
+                            subtitle: Text(notification['description'] ?? ''),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  icon: Icon(Icons.check, color: Colors.green),
+                                  onPressed: () {
+                                    // Handle mark as read
+                                    // Update the state if necessary
+                                  },
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.share, color: Colors.blue),
+                                  onPressed: () {
+                                    // Handle share functionality
+                                  },
+                                ),
+                              ],
                             ),
                           ),
-                          title: Text(
-                            notification['title'] ?? '',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                          subtitle: Text(notification['description'] ?? ''),
-                          trailing: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              IconButton(
-                                icon: Icon(Icons.check, color: Colors.green),
-                                onPressed: () {
-                                  // Handle mark as read
-                                },
-                              ),
-                              IconButton(
-                                icon: Icon(Icons.share, color: Colors.blue),
-                                onPressed: () {
-                                  // Handle share functionality
-                                },
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
+                        );
+                      },
+                    ),
                   ),
-                ),
+      ),
     );
   }
 }
